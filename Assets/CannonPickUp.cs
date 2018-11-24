c2aa7901e14f1265e7df3ddf45d4a668b8937128
Ryan M <fakegoldenstuff@gmail.com>
2018-11-23 18:11:23 -0600

    Added Cannon Pickup + broke some things
    Merging my branch broke level 2.
    But I think all the levels should be within one single scene anyway.
    
    The cannon script has been modified to work with a pickup prefab, and i've created CompleteLevels to test this (it'll eventually be modified to contain the whole game).

---
+++ Assets/CannonPickUp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonPickUp : MonoBehaviour {
+
+    public GameObject Player;
+    private Cannon cann;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cann = Player.GetComponent<Cannon>();	
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //checks the tag of the collider to see if it's the player.
+        if (other.tag == "Player")
+        {
+            cann.setPower(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //destroy the pickup once we know the player has the powerup.
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour {
+
+    public GameObject bullet;
+    private bool power;
+
+	// Use this for initialization
+	void Start () {
+        //prefab = Resources.Load("projectile") as GameObject;
+        power = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (power)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                //TODO
+                //DESTROY CANNONBALLS AFTER A WHILE
+                GameObject projectile = Instantiate(bullet) as GameObject;
+                projectile.transform.position = transform.position + Camera.main.transform.forward * 2;
+                Rigidbody rigbo = projectile.GetComponent<Rigidbody>();
+                rigbo.velocity = Camera.main.transform.forward * 56;
+            }
+        }
+    }
+
+    public void setPower(bool input)
+    {
+        power = input;
+    }
+
+}
