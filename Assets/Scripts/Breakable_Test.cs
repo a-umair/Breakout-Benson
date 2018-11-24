75b8148433baa02d31e5d515833f1ca287863f7d
getwellboxes <fakegoldenstuff@gmail.com>
2018-11-24 16:00:15 -0600

    Added Cheap BreakableWall
    Breakable wall changes with cannon damage, and breaks after 2 hits.

---
+++ Assets/Scripts/Breakable_Test.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable_Test : MonoBehaviour {
+
+    public int TotalHealth = 100;
+    private int health;
+    Renderer rend;
+
+    // Use this for initialization
+    void Start ()
+    {
+        health = TotalHealth;
+        rend = GetComponent<Renderer>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Projectile")
+        {
+            health = health - (TotalHealth/2);
+            Destroy(collision.gameObject);
+            //Set the main Color of the Material to red, to show it's damaged.
+            //actually broken atm.
+            rend.material.color = Color.red;
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        if(health != TotalHealth)
+        {
+            //do something regarding material color.
+        }
+	}
+}
