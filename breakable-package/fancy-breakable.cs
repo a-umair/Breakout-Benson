f78e13a65cfd30690ebd04362299770986f3f202
gamedev2042 <43999332+gamedev2042@users.noreply.github.com>
2018-11-25 00:50:02 -0600

    Uploaded breakable files
    Use C# script to spawn destroyed_block.fbx in place of Cube.prefab upon collision

---
+++ breakable-package/fancy-breakable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fancy-breakable : MonoBehaviour {
+	public GameObject destroyed_block;
+
+	public void blockDestroy(){
+
+		GameObject block_destruction = Instantiate(destroyed_block, this.transform.position, this.transform.rotation) as GameObject;
+		Destroy(gameObject);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(collision.collider.tag == "Projectile")
+        {
+            blockDestroy();
+        }
+	}
+}
