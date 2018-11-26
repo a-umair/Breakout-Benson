8ffae9add864615f7fda7c074e2e74970d9fbc74
Ryan M <fakegoldenstuff@gmail.com>
2018-11-25 19:18:29 -0600

    Implemented Fancy Breakable Wall
    Follows the same concept as before, but breaks into physical chunks, which can be shot 2 times to dissappear completely.
    
    Lots of instantiation and trash cleanup, but it works, I guess!

---
+++ Assets/Scripts/breakableparentTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class breakableparentTest : MonoBehaviour {
+
+    //This script is just here for cleanup.
+    //Whenever all the block chunks of the main block are destroyed,
+    //the parent object kills itself since it no longer has children.
+	void FixedUpdate () {
+		if(!gameObject.GetComponentInChildren<Rigidbody>())
+        {
+            Destroy(gameObject);
+        }
+	}
+}
---
+++ Assets/Scripts/fancyBreakable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fancyBreakable : MonoBehaviour {
+	public GameObject destroyed_block;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.collider.tag == "Projectile")
+        {
+            Vector3 velocity = collision.relativeVelocity; //get the velocity of the collision.
+            GameObject test = Instantiate(destroyed_block, this.transform.position + 2*Vector3.down, this.transform.rotation) as GameObject;//idk seems fishy.
+            Destroy(collision.gameObject);//kill the projectile
+            Destroy(gameObject);//kill self
+        }
+    }
+}
